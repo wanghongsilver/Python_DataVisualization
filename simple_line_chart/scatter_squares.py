067c0e3526527e09faf1dfe357ea26693c56981c
wanghongsilver <wanghong2016@foxmail.com>
2020-06-12 07:10:20 +0800

    删除数据点轮廓

--- simple_line_chart/scatter_squares.py
+++ simple_line_chart/scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x_value = list(range(1,1001))
 y_value = [x**2 for x in x_value]
 
-plt.scatter(x_value, y_value, s=200)
+plt.scatter(x_value, y_value, edgecolors='none', s=40)
 
 #设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
