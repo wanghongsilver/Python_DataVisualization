8dee4d0c6034eff517d40e5d78dd4b7247799c04
wanghongsilver <wanghong2016@foxmail.com>
2020-06-12 06:51:31 +0800

    简单散点图 自动计算数据，绘制一系列的点

--- simple_line_chart/scatter_squares.py
+++ simple_line_chart/scatter_squares.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-x_value = [1, 2, 3, 4, 5]
-y_value = [1, 4, 9, 16, 25]
+x_value = list(range(1,1001))
+y_value = [x**2 for x in x_value]
 
 plt.scatter(x_value, y_value, s=200)
 
@@ -12,5 +12,8 @@ plt.ylabel("Square of Value", fontsize=14)
 
 #设置刻度标记大小
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+#设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
