59eae3c580ff83abf4c073a3d368a8d77fce4e03
wanghongT490 <wanghong2016@foxmail.com>
2020-06-13 12:55:57 +0800

    项目目录维护，随机漫步

---
+++ Generate_data/simple_line_chart/mpl_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+#将参数导入，设置折线宽
+plt.plot(input_values, squares, linewidth=5)
+
+#设置图标标题，并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+#设置刻度标记大小
+plt.tick_params(axis='both', labelsize=14)
+
+
+plt.show()
+
+
