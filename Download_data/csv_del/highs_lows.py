cfef6432049e84dc856a744ed8ec8453587af018
wanghongT490 <wanghong2016@foxmail.com>
2020-06-22 06:03:25 +0800

    增加文件内容读取异常的判断

--- Download_data/csv_del/highs_lows.py
+++ Download_data/csv_del/highs_lows.py
@@ -2,7 +2,7 @@ import  csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = './input_data/sitka_weather_2014.csv'
+filename = './input_data/death_valley_2014.csv'
 
 #读取文件获得数据,日期 和 最高温度 最低气温
 with open(filename) as f:
@@ -11,15 +11,16 @@ with open(filename) as f:
 
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[2])
-        lows.append(low)
-    print(highs)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, "missing data")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 #根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -27,7 +28,7 @@ plt.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 #设置图形的格式
-plt.title("Daily high and low temperatures- 2014", fontsize=24)
+plt.title("Daily high and low temperatures- 2014\nDeath Valley,CA", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
