4c2abc17dca59fcc5d300c9c3f16d2c11b605103
wanghongT490 <wanghong2016@foxmail.com>
2020-06-22 05:22:36 +0800

    create 温度折线图

--- Download_data/csv_del/highs_lows.py
+++ Download_data/csv_del/highs_lows.py
@@ -1,8 +1,29 @@
 import  csv
+from datetime import datetime
+from matplotlib import pyplot as plt
 
-filename = ''
+filename = './input_data/sitka_weather_07-2014.csv'
 
+#读取文件获得数据
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
+
+#根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+#设置图形的格式
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
