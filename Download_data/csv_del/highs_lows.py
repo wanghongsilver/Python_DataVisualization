943c0ec1faad26c132b7f01de3b256b329ba3512
wanghongT490 <wanghong2016@foxmail.com>
2020-06-22 05:57:08 +0800

    增加最低温度获取，填充低温和高温着色

--- Download_data/csv_del/highs_lows.py
+++ Download_data/csv_del/highs_lows.py
@@ -2,28 +2,32 @@ import  csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = './input_data/sitka_weather_07-2014.csv'
+filename = './input_data/sitka_weather_2014.csv'
 
-#读取文件获得数据,日期 和 最高温度
+#读取文件获得数据,日期 和 最高温度 最低气温
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates,highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[2])
+        lows.append(low)
     print(highs)
 
 #根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
-
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 #设置图形的格式
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures- 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
